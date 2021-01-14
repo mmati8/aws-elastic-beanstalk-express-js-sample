955c1dc5a8d4274b3ee35d1a9a8fc5757d495669
mmati8 <mateusz.machnik@gmail.com>
2021-01-14 14:55:39 +0100

    Change name to Olga

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Mati!'));
+app.get('/', (req, res) => res.send('Hello Olga!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
